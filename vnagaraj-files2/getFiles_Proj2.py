33b80dd172330fd791e7495bb2e82d893ca2c9bd
Vindhya07 <vindhya.n2907@gmail.com>
2021-10-15 19:43:44 +0100

    second project

---
+++ vnagaraj-files2/getFiles_Proj2.py
@@ -0,0 +1,14 @@
+import requests
+import os
+
+url = 'https://cs7ns1.scss.tcd.ie/index.php/?shortname=vnagaraj&download=resume_speed'
+myFileNames = requests.get(url).text
+
+print(myFileNames)
+
+for i in myFileNames.split(',\n'):
+  file_url = 'https://cs7ns1.scss.tcd.ie/index.php/?shortname=vnagaraj&download=resume_speed&myfilename=' + i
+  myFiles = requests.get(file_url)
+  myFile = open(i, "wb")
+  myFile.write(myFiles.content)
+  myFile.close()
